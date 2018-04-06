26fd87ead88c3ea2ab2121ccf70da87ac03822a4
Vollcode <daniel.ortiz.olivares@gmail.com>
2018-04-06 14:16:33 +0300

    add database script creation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y apache2
-DEBIAN_FRONTEND=noninteractive sudo -E apt-get -q -y -f install mysql-server
-sudo apt-get -y install php5
+sudo apt-get install debconf-utils
+debconf-set-selections <<< "mysql-server mysql-server/root_password password mysql"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password mysql"
+sudo -E apt-get -q -y install mysql-server
 sudo apt-get -y install nodejs
 sudo apt-get -y install nodejs-legacy
-if ! [ -L /var/www/html/ ]; then
-  sudo rm -rf /var/www/html/
-  sudo ln -fs /vagrant /var/www/html
-fi
+sudo apt-get -y install npm
 
-echo 'CREATE DATABASE IF NOT EXISTS geekshubstravel CHARACTER SET Latin1 COLLATE latin1_spanish_ci;'> nodemysql.sql
-echo 'USE geekshubstravel;' >> nodemysql.sql
-echo 'CREATE TABLE user (id INT NOT NULL AUTO_INCREMENT,username VARCHAR(45) NOT NULL,email VARCHAR(45) NOT NULL,password VARCHAR(80) NOT NULL,hash VARCHAR(80) NULL,PRIMARY KEY (id));' >> nodemysql.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'10.0.2.2' IDENTIFIED BY 'mysql';" >> nodemysql.sql
-cat nodemysql.sql
-sudo mysql -u root < nodemysql.sql
+sudo mysql -uroot -pmysql < /vagrant/nodemysql.sql
